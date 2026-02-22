e39b93a00b648f632b93af49b4a7fd7ae5aa78ab
salman <saahmad@mtm-inc.net>
2026-02-22 15:17:38 +0500

    simple get request added for the home route

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 import express from "express";
 
 const app = express();
+const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send(body, 'Welcome to the Subsription Tracker API!');
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to the Subscription Tracker API!");
+});
 
+app.listen(PORT, () => {
+  console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
+});
+
+export default app;
