12c286eab7b9ccb4971dcc1f27293c087f051f0c
salman <salmancrc9@gmail.com>
2026-02-24 23:21:42 +0500

    sign in and sign out route added

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,3 +3,7 @@ import { Router } from "express";
 const authRouter = Router();
 
 authRouter.post(path, '/sign-up', (req, res) => res.send(body, { title: 'Sign Up' }));
+authRouter.post(path, '/sign-in', (req, res) => res.send(body, { title: 'Sign In' }));
+authRouter.post(path, '/sign-out', (req, res) => res.send(body, { title: 'Sign Out' }));
+
+export default authRouter;
