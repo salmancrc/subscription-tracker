761451365e59511cb172517e6f8cc23fd3ff9879
salman <salmancrc9@gmail.com>
2026-02-26 12:23:57 +0500

    auth, users, subscriptions routes use in the app.js

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 import express from "express";
 import { PORT } from './config/env.js'
 
-const app = express()
+import userRouter from './routes/user.routers.js'
+import authRouter from './routes/auth.routes.js'
+import subscriptionRouter from './routes/subscription.routes.js'
+
+const app = express();
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API!");
