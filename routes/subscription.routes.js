8580aa97f1210e8604f004cc0c3d5c15ed4b9504
salman <salmancrc9@gmail.com>
2026-02-27 23:27:04 +0500

    subscription routes introduced with Cancel and Upcoming routes added

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -4,4 +4,18 @@ const subscriptionRouter = Router();
 
 subscriptionRouter.get('/', (req, res) => res.send({ title: 'GET all subscriptions' }));
 
+subscriptionRouter.get('/:id', (req, res) => res.send({ title: 'GET subscriptions details' }));
+
+subscriptionRouter.post('/', (req, res) => res.send({ title: 'CREATE subscription' }));
+
+subscriptionRouter.put('/:id', (req, res) => res.send({ title: 'UPDATE subscription' }));
+
+subscriptionRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE subscription' }));
+
+subscriptionRouter.get('/user/:id', (req, res) => res.send({ title: 'GET all user subscriptions' }));
+
+subscriptionRouter.put('/:id/cancel', (req, res) => res.send({ title: 'CANCEL subscription' }));
+
+subscriptionRouter.put('/upcoming-renewals', (req, res) => res.send({ title: 'GET upcoming renewals' }));
+
 export default subscriptionRouter;
