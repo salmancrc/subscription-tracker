557f3dbc90b4e5cd616ff54d437d36a77a889392
salman <salmancrc9@gmail.com>
2026-03-02 15:35:18 +0500

    mongo db connection established and DB_URI added in env

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import { PORT } from './config/env.js'
 import userRouter from './routes/user.routers.js'
 import authRouter from './routes/auth.routes.js'
 import subscriptionRouter from './routes/subscription.routes.js'
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 
@@ -15,8 +16,11 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async() => {
   console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
+
+  connectToDatabase();
 });
+
 
 export default app;
