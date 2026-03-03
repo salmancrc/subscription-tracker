7456b3f7ab309860b65d02fb91d993ab14701cea
salman <salmancrc9@gmail.com>
2026-03-03 23:49:45 +0500

    email and password schema added in model

--- models/user.model.js
+++ models/user.model.js
@@ -8,6 +8,19 @@ const userSchema = new mongoose.Schema({
     minLength: 2,
     maxLength: 50,
   },
+  email: {
+    type: String,
+    required: [true, 'User Email is required'],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [/\S+@\S+\.\S+/, 'Please fill a valid email address'],
+  },
+  password: {
+    type: String,
+    required: [true, 'User Password is required'],
+    minLength: 6,
+  }
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
