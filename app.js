d682f2e45e492f4615fdfc7fa4d0e4c0b24ee56f
salman <salmancrc9@gmail.com>
2026-03-07 12:32:18 +0500

    use json, urlencoded, cookieParser middlewares

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ import errorMiddleware from "./middleware/error.middleware.js";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
