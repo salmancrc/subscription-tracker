fdb51ab133380c88a277a59836529705de9c4d79
salman <salmancrc9@gmail.com>
2026-03-25 23:15:47 +0500

    auth controllers imported

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { signUp } from "../config/controllers/auth.controller";
+import { signIn, signOut, signUp } from "../config/controllers/auth.controller.js";
 
 const authRouter = Router();
 
 authRouter.post('/sign-up', (req, res) => res.send({ title: 'Sign Up',  signUp}));
-authRouter.post('/sign-in', (req, res) => res.send({ title: 'Sign In' }));
-authRouter.post('/sign-out', (req, res) => res.send({ title: 'Sign Out' }));
+authRouter.post('/sign-in', (req, res) => res.send({ title: 'Sign In', signIn }));
+authRouter.post('/sign-out', (req, res) => res.send({ title: 'Sign Out', signOut }));
 
 export default authRouter;
