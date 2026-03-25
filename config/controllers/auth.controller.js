7ed211850c3be208259ba953bbf72b669f6f2178
salman <salmancrc9@gmail.com>
2026-03-25 23:17:14 +0500

    mongoose sessions handling in the auth controller

--- config/controllers/auth.controller.js
+++ config/controllers/auth.controller.js
@@ -1,5 +1,20 @@
+import mongoose from "mongoose"
+import User from "../../models/user.model.js";
+import pkg from "jsonwebtoken";
+
 export const signUp = async (req, res, next) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  const { jwt } = pkg;
+
+  try {
+    const { name, email, password } = req.body;
   
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    next(error); 
+  }
 }
 
 export const signIn = async (req, res, next) => { 
