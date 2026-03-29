8ec1794103356b41a6c237608644903ee6167a40
salman <salmancrc9@gmail.com>
2026-03-29 22:53:18 +0500

    fixed the statusCode issue with relevant codes

--- middleware/error.middleware.js
+++ middleware/error.middleware.js
@@ -12,21 +12,21 @@ const errorMiddleware = (err, req, res, next) => {
     if (err.name === 'CastError') {
       const message = 'Resource not found';
       error = new Error(message);
-      error.StatusCode = 404;
+      error.statusCode = 404;
     }
 
     // Mongoose duplicate key
     if (err.code === 11000) {
       const message = 'Duplicate field value entered';
       error = new Error(message);
-      error.StatusCode = 400;
+      error.statusCode = 400;
     }
 
     // Mongoose validation error
     if (err.name === 'ValidationError') {
       const message = Object.values(err.errors).map(val => val.message);
       error = new Error(message.join(', '));
-      error.StatusCode = 400;
+      error.statusCode = 400;
     }
 
     res.status(error.StatusCode || 500).json({
