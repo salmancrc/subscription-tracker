6475730dc7e2751b80ff88ecb3577d11be81db2d
salman <salmancrc9@gmail.com>
2026-04-01 16:01:14 +0500

    getUsers and getUser controller added in APIs handler

--- routes/user.routers.js
+++ routes/user.routers.js
@@ -1,15 +1,12 @@
 import { Router } from "express";
+import { getUsers, getUser } from "../config/controllers/user.controller.js";
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => res.send({ title: 'GET all users' }));
-
-userRouter.get('/:id', (req, res) => res.send({ title: 'GET users details' }));
-
+userRouter.get('/', getUsers);
+userRouter.get('/:id', getUser);
 userRouter.post('/', (req, res) => res.send({ title: 'CREATE new user' }));
-
 userRouter.put('/:id', (req, res) => res.send({ title: 'UPDATE user' }));
-
 userRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE user' }));
 
 export default userRouter;
