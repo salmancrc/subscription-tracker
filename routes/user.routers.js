77cd9a252df50fe43b57c54daa174d3ab4029cee
salman <salmancrc9@gmail.com>
2026-04-02 23:42:31 +0500

    authorize and error check middleware added for the user routes to secure single user details

--- routes/user.routers.js
+++ routes/user.routers.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import authorize from '../middleware/auth.middleware.js'
+import errorMiddleware from "../middleware/error.middleware.js";
 import { getUsers, getUser } from "../config/controllers/user.controller.js";
 
 const userRouter = Router();
 
 userRouter.get('/', getUsers);
-userRouter.get('/:id', getUser);
+userRouter.get('/:id', authorize, errorMiddleware, getUser);
 userRouter.post('/', (req, res) => res.send({ title: 'CREATE new user' }));
 userRouter.put('/:id', (req, res) => res.send({ title: 'UPDATE user' }));
 userRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE user' }));
