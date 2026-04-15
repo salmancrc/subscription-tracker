228b125de35495042e0d9998ed8fc4b8c10619f8
salman <salmancrc9@gmail.com>
2026-04-15 23:55:25 +0500

    implement createSubscription controller function to handle subscription creation

--- config/controllers/subscription.controller.js
+++ config/controllers/subscription.controller.js
@@ -2,7 +2,12 @@ import Subscription from "../../models/subscription.model.js";
 
 export const createSubscription = async (req, res, next) => {
   try {
+    const subscription = await Subscription.create({
+      ...req.body,
+      user: req.user._id,
+    });
 
+    res.status(201).json({ success: true, data: subscription }); 
   } catch (e) {
     next(e);
   }
