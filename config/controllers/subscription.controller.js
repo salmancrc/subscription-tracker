49a98afee1c060a6afc5d8528570b18bf9a698d6
salman <salmancrc9@gmail.com>
2026-05-03 20:02:19 +0500

    get single user subscription details

--- config/controllers/subscription.controller.js
+++ config/controllers/subscription.controller.js
@@ -12,3 +12,19 @@ export const createSubscription = async (req, res, next) => {
     next(e);
   }
 }
+
+export const getUserSubscription = async (req, res, next) => {
+  try {
+    if (req.user.id !== req.params.id) {
+      const error = new Error("You are not the awner of this account");
+      error.status = 401;
+      throw error;
+    }
+
+    const subscriptions = await Subscription.find({ user: req.params.id });
+
+    res.status(200).json({ success: true, data: subscriptions });
+  } catch (e) {
+    next(e)
+  }
+}
